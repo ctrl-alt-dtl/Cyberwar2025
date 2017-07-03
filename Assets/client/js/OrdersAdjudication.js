b58239c1e40d4a2143f9f4629d6aed2ba40ab8a6
David Ty Long <dtlong@nps.edu>
2017-07-02 18:36:29 -0700

    Creating Wiki for Tracking and Documentation Notes

---
+++ Assets/client/js/OrdersAdjudication.js
@@ -0,0 +1,6 @@
+/**
+ * File: OrdersAdjudication.js
+ * Created by: David "Ty" Long (Math developed by: Niels Blom Areth)
+ * Date: 7/2/17
+ * Info: Used for mathematically equating the actions of players' move using the "Niels' Method".
+ */
