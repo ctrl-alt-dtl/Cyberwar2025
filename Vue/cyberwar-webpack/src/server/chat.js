1f595af33b79317bf0983a67b262e15a9aee1d3f
David Ty Long <dtlong@nps.edu>
2017-07-11 11:59:49 -0700

    Initial add of files.

---
+++ Vue/cyberwar-webpack/src/server/chat.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ty on 7/10/17.
+ */
---
+++ Vue/cyberwar-webpack/src/server/database.js
@@ -0,0 +1,9 @@
+/**
+ * File: database.js
+ * Created by: David "Ty" Long
+ * Date: 7/10/17
+ * Info: This is the server side code to control and record game states.
+ */
+
+var mongodb = require("mongodb");
+var log = require("./log").log;
