f6e6fb26dcea451e7d2c5e3cc6b1f831b5c8e03c
David Ty Long <dtlong@nps.edu>
2017-08-31 16:32:40 -0700

    Reworking Konva into React.

--- React/cyberwar-react/src/App.js
+++ React/cyberwar-react/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import {Stage, Layer, Group} from 'react-konva';
+import Canvas from "../src/client/js/Canvas/Canvas"
 import logo from './logo.svg';
 import './App.css';
-
-var BASE_BOARD_WIDTH = 1280;
-var BASE_BOARD_HEIGHT = 960;
-var DOMAIN_LAYER_BOARD_ROTATION = 0;
 
 class App extends Component {
   render() {
     return (
-
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -19,27 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Group ref="domainsGroup" width={BASE_BOARD_WIDTH}
-               height={BASE_BOARD_HEIGHT}
-               x={BASE_BOARD_WIDTH / 2}
-               y={BASE_BOARD_HEIGHT / 2}
-               offsetx={BASE_BOARD_WIDTH / 2}
-               offsety={BASE_BOARD_HEIGHT / 2}
-               rotation={DOMAIN_LAYER_BOARD_ROTATION}>
-          <Stage ref="stage" width={BASE_BOARD_WIDTH} height={BASE_BOARD_HEIGHT}>
-            <Layer ref="bgLayer">
-            </Layer>
-            <Layer ref="bgItems">
-            </Layer>
-            <Layer ref="domainsLayer">
-            </Layer>
-            <Layer ref="toolTipLayer">
-            </Layer>
-          </Stage>
-        </Group>
+        <div className="Canvas">
+          <Canvas/>
+        </div>
       </div>
-
-
     );
   }
 }
---
+++ React/cyberwar-react/src/client/js/Canvas/Canvas.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+// import CanvasElements from '../canvasElements/canvasElements';
+
+class Canvas extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Canvas;
