1fe07cec125c6b7436746d4d3b0f98e873a74ed6
Ty Long <dtylerlong@gmail.com>
2017-09-28 10:56:46 -0700

    Added Konva canvas functionality into Angular 1 framework.
    It Works!!

--- Angular/cyberwar-ang1/app/app.js
+++ Angular/cyberwar-ang1/app/app.js
@@ -2,13 +2,18 @@
 
 var app = angular.module('myApp', []);
 
+var BASE_BOARD_WIDTH = 1280;
+var BASE_BOARD_HEIGHT = 960;
+
+var DOMAIN_LAYER_BOARD_ROTATION = 0;
+
 app.controller('canvasCtrl', function($scope){
   $scope.canvasKonvaObj = false;
   $scope.canvaskonvaStageObj = false;
   $scope.isDraggable = true;
 });
 
-app.directive('draggableCanvas', ['$rootScope', function ($rootScope) {
+app.directive('gameBoard', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'A',
     scope: {
@@ -27,14 +32,95 @@ app.directive('draggableCanvas', ['$rootScope', function ($rootScope) {
         if (!scope.konvastageobj) {
           scope.konvastageobj = new Konva.Stage({
             container: id,
-            width: 578,
-            height: 200
+            width: BASE_BOARD_WIDTH,
+            height: BASE_BOARD_HEIGHT
           });
         }
         if (!scope.konvastageobj.container) {
           scope.konvastageobj.attrs.container = id;
         }
       }
+
+      var centerHex = new Konva.RegularPolygon({
+        x: scope.konvastageobj.width() / 2,
+        y: scope.konvastageobj.height() / 2,
+        sides: 6,
+        radius: BASE_BOARD_WIDTH / 38,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 2,
+        rotation: 90
+      });
+
+      var boardEdge= new Konva.RegularPolygon({
+        x: scope.konvastageobj.width() / 2,
+        y: scope.konvastageobj.height() / 2,
+        sides: 6,
+        radius: BASE_BOARD_WIDTH / 2.79,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 4,
+        rotation: 90
+      });
+
+      var tier3= new Konva.RegularPolygon({
+        x: scope.konvastageobj.width() / 2,
+        y: scope.konvastageobj.height() / 2,
+        sides: 6,
+        radius: BASE_BOARD_WIDTH / 3.5,
+        fill: '#E3E3E3',
+        stroke: 'black',
+        strokeWidth: 1,
+        rotation: 90
+      });
+
+      var tier2= new Konva.RegularPolygon({
+        x: scope.konvastageobj.width() / 2,
+        y: scope.konvastageobj.height() / 2,
+        sides: 6,
+        radius: BASE_BOARD_WIDTH / 5,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 1,
+        rotation: 90
+      });
+
+      var tier1= new Konva.RegularPolygon({
+        x: scope.konvastageobj.width() / 2,
+        y: scope.konvastageobj.height() / 2,
+        sides: 6,
+        radius: BASE_BOARD_WIDTH / 9,
+        fill: '#E3E3E3',
+        stroke: 'black',
+        strokeWidth: 1,
+        rotation: 90
+      });
+
+      var bgLayer = new Konva.Layer();
+      var boardItems = new Konva.Layer();
+      var domainsLayer = new Konva.Layer();
+      var toolTipLayer = new Konva.Layer();
+
+      var domainsGroup = new Konva.Group({
+        width: BASE_BOARD_WIDTH,
+        height: BASE_BOARD_HEIGHT,
+        x: BASE_BOARD_WIDTH / 2,
+        y: BASE_BOARD_HEIGHT / 2,
+        offset: {
+          x: BASE_BOARD_WIDTH / 2,
+          y: BASE_BOARD_HEIGHT / 2,
+        },
+        rotation: DOMAIN_LAYER_BOARD_ROTATION
+      });
+
+      // add the shapes to the layer, the order matters (background, links, bases, then servers).
+      bgLayer.add(boardEdge, tier3, tier2, tier1);
+      bgLayer.add(minorBounds1, minorBounds2, minorBounds3, minorBounds4, minorBounds5, minorBounds6);
+      bgLayer.add(mainBoundary3, mainBoundary2, mainBoundary1, centerHex);
+      bgLayer.add(tier3Text, tier2Text, tier1Text);
+      // Extra board items (Windows, Buttons, Chat, and Actions)
+      boardItems.add(submitOrdersBtn, ordersQueue, effectsWindow, chatWindow, notesRulesWindow);
+
       var layer = new Konva.Layer();
       var rectX = scope.konvastageobj.getWidth() / 2 - 50;
       var rectY = scope.konvastageobj.getHeight() / 2 - 25;
@@ -68,7 +154,7 @@ app.directive('draggableCanvas', ['$rootScope', function ($rootScope) {
       });
 
       layer.add(scope.konvaobj);
-      scope.konvastageobj.add(layer);
+      scope.konvastageobj.add(bgLayer, boardItems);
 
     }
   }
---
+++ Angular/cyberwar-ang1/app/assets/client/js/orangeDomain/orangeDomainServers.js
@@ -0,0 +1,127 @@
+/**
+ * ORANGE DOMAIN
+ */
+/**
+ * ORANGE DOMAIN
+ * Requires:
+ *    ../domainLibs/konvaConfigSettings.js
+ *    ../domainLibs/serverPointCoordinates.js
+ */
+// Orange Base Outline
+var oBase = new Konva.Line({
+  points: [
+    pt_oBase1Hex_X,       // X1
+    pt_oBase1Hex_Y,      // Y1
+    point_x / 1.44,        // X2
+    point_y / 1.28,      // Y2
+    point_x / 1.28,       // X3
+    point_y / 1.715,       // Y3
+    pt_oBase2Hex_X,       // X4
+    pt_oBase2Hex_Y,       // Y4
+    point_x / 1.237,        // X5
+    point_y / 1.652,       // Y5
+    point_x / 1.387,       // X6
+    point_y / 1.24         // Y6
+  ],
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  closed: true
+});
+
+var o1Hex = new Konva.RegularPolygon({
+  x: pt_o1Hex_X,
+  y: pt_o1Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o1Hex'
+});
+
+var o2Hex = new Konva.RegularPolygon({
+  x: pt_o2Hex_X,
+  y: pt_o2Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o2Hex'
+});
+
+var o3Hex = new Konva.RegularPolygon({
+  x: pt_o3Hex_X,
+  y: pt_o3Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o3Hex'
+});
+
+var o4Hex = new Konva.RegularPolygon({
+  x: pt_o4Hex_X,
+  y: pt_o4Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o4Hex'
+});
+
+var o5Hex = new Konva.RegularPolygon({
+  x: pt_o5Hex_X,
+  y: pt_o5Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o5Hex'
+});
+
+var o6Hex = new Konva.RegularPolygon({
+  x: pt_o6Hex_X,
+  y: pt_o6Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o6Hex'
+});
+
+var o7Hex = new Konva.RegularPolygon({
+  x: pt_o7Hex_X,
+  y: pt_o7Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o7Hex'
+});
+
+var o8Hex = new Konva.RegularPolygon({
+  x: pt_o8Hex_X,
+  y: pt_o8Hex_Y,
+  sides: 6,
+  radius: radius,
+  fill: 'orange',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotation: 90,
+  id: 'o8Hex'
+});
+
