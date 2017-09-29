71c3031f73e038017045b1670804512520c4535e
David Ty Long <dtlong@nps.edu>
2017-09-28 17:33:07 -0700

    Server points are in the proper location and on the top layer. This means that the other points and links will follow in suit.

--- Angular/cyberwar-ang1/app/app.js
+++ Angular/cyberwar-ang1/app/app.js
@@ -16,6 +16,7 @@ const MIN_BOARD_HEIGHT = 600;
 
 var DOMAIN_LAYER_BOARD_ROTATION = 0;
 
+
 var stage = new Konva.Stage({
   container: '.container',
   width: BASE_BOARD_WIDTH,
@@ -24,6 +25,19 @@ var stage = new Konva.Stage({
 
 var bgLayer = new Konva.Layer();
 var boardItems = new Konva.Layer();
+var domainsLayer = new Konva.Layer();
+
+var domainsGroup = new Konva.Group({
+  width: BASE_BOARD_WIDTH,
+  height: BASE_BOARD_HEIGHT,
+  x: BASE_BOARD_WIDTH / 2,
+  y: BASE_BOARD_HEIGHT / 2,
+  offset: {
+    x: BASE_BOARD_WIDTH / 2,
+    y: BASE_BOARD_HEIGHT / 2,
+  },
+  rotation: DOMAIN_LAYER_BOARD_ROTATION
+});
 
 var imageObj = new Image();
 // background board
@@ -44,8 +58,8 @@ imageObj.onload = function() {
   // Extra board items (Windows, Buttons, Chat, and Actions)
   boardItems.add(submitOrdersBtn, ordersQueue, effectsWindow, chatWindow, notesRulesWindow);
 
-  // domainsLayer.add(domainsGroup);
-  stage.add(bgLayer,boardItems);
+  domainsLayer.add(domainsGroup);
+  stage.add(bgLayer,boardItems, domainsLayer);
 
 };
 
@@ -60,7 +74,7 @@ app.controller('canvasCtrl', function($scope){
 
 console.log("beforeDirective");
 
-app.directive('gameBoardServers', ['$rootScope', function ($rootScope) {
+app.directive('gameBoardRedServers', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'A',
     scope: {
@@ -68,7 +82,7 @@ app.directive('gameBoardServers', ['$rootScope', function ($rootScope) {
       konvastageobj: '='
     },
     link: function (scope, el, attrs) {
-      console.log("gameBoardLinksLoading");
+      console.log("gameBoardRedServersLoading");
       if (!scope.konvastageobj) {
         var id = attrs["id"];
         //create random unique id
@@ -83,11 +97,10 @@ app.directive('gameBoardServers', ['$rootScope', function ($rootScope) {
         }
       }
 
-      var domainsLayer = new Konva.Layer();
-
       // domainsGroup.add(origin);
       // add the layer to the stage -- 4 for now
-      domainsLayer.add(r1Hex, r2Hex, r3Hex, r4Hex, r5Hex, r6Hex, r7Hex, r8Hex);
+      domainsGroup.add(r1Hex, r2Hex, r3Hex, r4Hex, r5Hex, r6Hex, r7Hex, r8Hex);
+      domainsLayer(domainsGroup);
 
       /*var layer = new Konva.Layer();
       var rectX = scope.konvastageobj.getWidth() / 2 - 50;
@@ -123,9 +136,76 @@ app.directive('gameBoardServers', ['$rootScope', function ($rootScope) {
 
       layer.add(scope.konvaobj);*/
 
-      console.log("gameBoardLinksLoaded")
+      console.log("gameBoardRedServersLoaded")
       scope.konvastageobj.add(domainsLayer);
 
+    }
+  }
+}]);
+
+app.directive('gameBoardPurpleServers', ['$rootScope', function ($rootScope) {
+  return {
+    restrict: 'A',
+    scope: {
+      konvaobj: '=',
+      konvastageobj: '='
+    },
+    link: function (scope, el, attrs) {
+      console.log("gameBoardPurpleServersLoading");
+      if (!scope.konvastageobj) {
+        var id = attrs["id"];
+        //create random unique id
+        if (!id) {
+          id = Math.random().toString(36).substring(7);
+        }
+        if (!scope.konvastageobj) {
+          scope.konvastageobj = stage;
+        }
+        if (!scope.konvastageobj.container) {
+          scope.konvastageobj.attrs.container = id;
+        }
+      }
+
+      // add the layer to the stage -- 4 for now
+      domainsGroup.add(p1Hex, p2Hex,p3Hex, p4Hex, p5Hex, p6Hex, p7Hex, p8Hex);
+      domainsLayer(domainsGroup);
+
+      /*var layer = new Konva.Layer();
+      var rectX = scope.konvastageobj.getWidth() / 2 - 50;
+      var rectY = scope.konvastageobj.getHeight() / 2 - 25;
+
+      //if kineticObj is null, init
+      var options = {
+        x: rectX,
+        y: rectY,
+        width: 100,
+        height: 50,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+      };
+      if (scope.isdraggable) {
+        options.draggable = true;
+      }
+      if (!scope.konvaobj) {
+        scope.konvaobj = new Konva.Rect(options);
+      }
+
+      // add cursor styling
+      scope.konvaobj.on('mouseover', function () {
+
+        document.body.style.cursor = 'pointer';
+
+      });
+      scope.konvaobj.on('mouseout', function () {
+        document.body.style.cursor = 'default';
+        $rootScope.$emit("CANVAS-MOUSEOUT");
+      });
+
+      layer.add(scope.konvaobj);*/
+
+      console.log("gameBoardPurpleServersLoaded")
+      // scope.konvastageobj.add(domainsLayer);
 
     }
   }
