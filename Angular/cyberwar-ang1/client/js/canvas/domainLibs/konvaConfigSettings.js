b5986ac21ed5308076001d57896cabc8ab51f2b9
David "Ty" Long <dtlong@nps.edu>
2019-07-12 21:42:38 -0700

    Color scheme change. Removed some old files that were not needed anymore.

--- Angular/cyberwar-ang1/client/js/canvas/domainLibs/konvaConfigSettings.js
+++ Angular/cyberwar-ang1/client/js/canvas/domainLibs/konvaConfigSettings.js
@@ -8,13 +8,13 @@ var lineVisible = true;
 var textVisible = true;
 
 // Used for the Domain Servers and Links
-var yellowColor = '#F7E500';
-var darkYellowColor = '#CCBE00'
-var blueColor = 'blue';
-var redColor = 'red';
-var purpleColor = 'purple';
-var greenColor = 'green';
-var orangeColor = 'orange';
+//var yellowColor = '#F7E500';
+var yellowColor = '#E5CE01'
+var blueColor = '#1E4AA8';
+var redColor = '#D91C35';
+var purpleColor = '#79368F';
+var greenColor = '#008C1D';
+var orangeColor = '#E06609';
 var grayColor = 'gray';
 var noColor = '';
 
